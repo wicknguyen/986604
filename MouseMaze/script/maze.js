661b88b3f5a1da01f07c58ab23405bd68c57ac7c
wicknguyen <nguyenquy8321@gmail.com>
2019-05-16 20:11:52 -0500

    - Mouse Maze.

---
+++ MouseMaze/script/maze.js
@@ -0,0 +1,49 @@
+/*jshint esversion: 6 */
+/* globals $ */
+$(document).ready(function () {
+    var win = true;
+    var gameStarted = false;
+
+    let startEvent = function () {
+        if (!gameStarted) {
+            gameStarted = true;
+        } else {
+            gameStarted = false;
+            win = true;
+        }
+    };
+
+    let removeLoseWarning = function () {
+        $("#maze .boundary").removeClass("youlose");
+    };
+
+    let addLoseWarning = function () {
+        $("#maze .boundary").addClass("youlose");
+    };
+
+    let loseWarning = function () {
+        if (gameStarted) {
+            addLoseWarning();
+            win = false;
+        }
+    };
+
+    let endGame = function () {
+        if(!gameStarted) {
+            return;
+        }
+        if (win) {
+            $("#status").text("You win!:]");
+        } else {
+            $("#status").text("You lose!!!");
+        }
+
+    };
+
+    $("#start").click(startEvent);
+    $("#end").mouseover(endGame);
+    $("#maze .boundary").mouseover(loseWarning);
+    $("#maze .boundary").mouseout(removeLoseWarning);
+    $("#maze").mouseleave(loseWarning);
+});
+
