930443b339e589cb62578f4920c862340bd36de7
wicknguyen <nguyenquy8321@gmail.com>
2019-05-17 23:50:02 -0500

    - Fifteen Puzzle

---
+++ FifteenPuzzle/scripts/fifteen.js
@@ -0,0 +1,90 @@
+/*jshint esversion: 6 */
+/* globals $ */
+$(document).ready(function () {
+    let blankTilePos = 15;
+
+    let calculateX = function(position) {
+        return ((position % 4) * 100);
+    }
+
+    let calculateY = function(position) {
+        return (Math.floor(position / 4) * 100);
+    }
+
+    let canMoved = function(element) {
+        let x = parseInt($(element).css('left'));
+        let y = parseInt($(element).css('top'));
+        let distanceX = Math.abs(x - calculateX(blankTilePos));
+        let distanceY = Math.abs(y - calculateY(blankTilePos));
+
+        return distanceX === 100 && distanceY === 0
+            || distanceX === 0 && distanceY === 100;
+    }
+
+    let moveTile = function(element) {
+        if (canMoved(element)) {
+            let tmp = blankTilePos;
+            blankTilePos = parseInt($(element).attr('id'));
+            $(element).css('left', calculateX(tmp) + 'px');
+            $(element).css('top', calculateY(tmp) + 'px');
+            $(element).attr('id', tmp);
+        }
+    }
+
+    let setBackground = function(divElement, index) {
+        $(divElement).css('background-image', 'url("./images/background.jpg")');
+        $(divElement).css('background-position', -calculateX(index) + 'px ' + (-calculateY(index)) + 'px');
+    }
+
+    let arrangeTile = function(divElement, index) {
+        // calculate x and y for this piece
+        let newIndex = index;
+        if (index >= blankTilePos) {
+            newIndex = index + 1;
+        }
+        let x = calculateX(newIndex);
+        let y = calculateY(newIndex);
+        // set basic style and background
+        $(divElement).css('left', x + 'px');
+        $(divElement).css('top', y + 'px');
+        $(divElement).text(newIndex);
+        $(divElement).attr('id', newIndex);
+    }
+
+    let init = function() {
+        // initialize each piece
+        $("#puzzlearea div").each(function (index) {
+            $(this).addClass("puzzlepiece");
+            arrangeTile(this, index);
+            setBackground(this, index);
+        });
+
+        // Hover event
+        $("#puzzlearea div").hover(function () {
+            if (canMoved(this)) {
+                $(this).addClass("movablepiece");
+            }
+        }, function () {
+            $(this).removeClass("movablepiece");
+        });
+
+        $("#puzzlearea div").click(function () {
+            moveTile(this);
+        });
+
+        $("#shufflebutton").click(function () {
+            blankTilePos = parseInt(Math.random() * 15);
+            $("#puzzlearea div").each(function (index) {
+                arrangeTile(this, index);
+            });
+            for (let i = 0; i < 7; i++) {
+                $("#puzzlearea div").each(function (index) {
+                    moveTile(this);
+                });
+            }
+        });
+    };
+
+    init();
+});
+
